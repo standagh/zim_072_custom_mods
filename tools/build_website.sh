9e0131f2cef90c130ef6aa1536ee802b11710673
Standa Mitrega <stanislav.mitrega@embedit.cz>
2019-12-19 23:03:06 +0100

    initial_commit

---
+++ tools/build_website.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -fr './html'
+
+./zim.py --export ./website/pages/ -V \
+	--template ./website/template.html \
+	--output ./html/ \
+	--root-url /
+
+./zim.py --export ./data/manual/ -V \
+	--template ./website/template.html \
+	--output ./html/manual/ \
+	--root-url /
+
+cp -R website/files/* html/
+
+# HACKs to include raw HTML
+perl -i -pe 's{INSERT_PAYPAL_BUTTON_HERE}{<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="11297015">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
+</form>}' html/contribute.html
